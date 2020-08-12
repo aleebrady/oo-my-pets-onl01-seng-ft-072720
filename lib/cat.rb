d7fc1026163137b4eb1ee4e7b2a62ca17299da8d
Aaron Brady <aleebrady@gmail.com>
2020-08-12 17:52:25 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Cat
   
-  attr_accessor :owner 
+  attr_accessor :owner :mood
   attr_reader :name # cannot change cat's name
   
   def initialize(name, owner) #initilizes with name and owner 
