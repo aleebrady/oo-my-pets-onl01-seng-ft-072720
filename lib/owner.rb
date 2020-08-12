bbe84293b2cd97ae02805619a14aa8320170d2ea
Aaron Brady <aleebrady@gmail.com>
2020-08-12 17:18:50 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,10 @@
 class Owner
   
-  attr_reader :name # cannot change owner's name
+  attr_reader :name, :species # cannot change owner's name
   
   def initialize(name)
-    @name = name 
+    @name = name
+    species = human
   end
   
 end
