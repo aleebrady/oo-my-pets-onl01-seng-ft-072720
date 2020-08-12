49847b2c64c6b76c42fae59c01ff6232ecbe6449
Aaron Brady <aleebrady@gmail.com>
2020-08-12 17:26:58 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,13 +2,13 @@ class Owner
   
   attr_reader :name, :species # cannot change owner or species
   
-  def initialize(name)
+  def initialize(name) #initializes with a name
     @name = name
-    @species = "human"
+    @species = "human" #initializes with a species 
   end
   
   def say_species
-    return "I am a #{self.species}."
+    return "I am a #{self.species}." #can specify it's species when object is created 
   end
   
 end
