e4357da065beb2646e7708d2c45c9a252bca9b5e
Aaron Brady <aleebrady@gmail.com>
2020-08-12 17:28:02 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -4,11 +4,11 @@ class Owner
   
   def initialize(name) #initializes with a name
     @name = name
-    @species = "human" #initializes with a species 
+    @species = "human" #initializes with a species = human
   end
   
   def say_species
-    return "I am a #{self.species}." #can specify it's species when object is created 
+    return "I am a #{self.species}." #can specify it's species when created of .self
   end
   
 end
